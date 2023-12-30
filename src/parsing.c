c41908bf656044c0799f8ae5976faea5d50b364b
lucasDS-0 <90753591+lucasDS-0@users.noreply.github.com>
2023-12-30 02:36:05 -0300

    ch7 done

--- src/parsing.c
+++ src/parsing.c
@@ -25,6 +25,38 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+    if (strstr(op, "+") == 0) { return x + y; }
+    if (strstr(op, "-") == 0) { return x - y; }
+    if (strstr(op, "*") == 0) { return x * y; }
+    if (strstr(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+    /* If tagged as number return it directly */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+    
+    /* The operator is always second child */
+    char* op = t->children[1]->contents;
+    
+    /* We store the third child in 'x' */
+    long x = eval(t->children[2]);
+    
+    /* Iterate the remaining children and combining */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    
+    return x;    
+}
+
 int main(int argc, char** argv) {
 
     /* Create Some Parser */
@@ -37,13 +69,13 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
     "                                                      \
         number   : /-?[0-9]+(.[0-9])?/ ;                            \
-        operator : '-' | '+' | '*' | '/' | '%' ;                 \
+        operator : '-' | '+' | '*' | '/' ;                 \
         expr     : <number> | '(' <operator> <expr>+ ')' ; \
         lips     : /^/ <operator> <expr>+ /$/ ;            \
     ",
     Number, Operator, Expr, Lips);
 
-    puts("Lips Version 0.0.0.0.2");
+    puts("Lips Version 0.0.0.0.3");
     puts("Press Ctrl+c to Exit this program\n");
     
     while (1) {
@@ -56,19 +88,21 @@ int main(int argc, char** argv) {
         /* Attempt to Parse the user Input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lips, &r)) {
-            /* On Success Print the AST */
-            mpc_ast_print(r.output);
+            /* On Success Print the Result */
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             /* Otherwise Print the Error */
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
+        
         free(input);
         
     }
     
-    /* Udefine and Delete our Parsers */
+    /* Undefine and Delete our Parsers */
     mpc_cleanup(4, Number, Operator, Expr, Lips);
     
     return 0;
