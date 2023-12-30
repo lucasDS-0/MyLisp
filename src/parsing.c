70d0368299f2e106b87e2aca09b4302806d09ff8
lucasDS-0 <90753591+lucasDS-0@users.noreply.github.com>
2023-12-29 21:40:30 -0300

    added mpc to repo

---
+++ src/parsing.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mpc.h"
+
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+int main(int argc, char** argv) {
+    
+    puts("Lips Version 0.0.0.0.2");
+    puts("Press Ctrl+c to Exit this program\n");
+    
+    while (1) {
+
+        /* Now in either case readline will be correctly defined */
+        char* input = readline("lips> ");
+        add_history(input);
+                
+        printf("Indeed, %s\n", input);
+        free(input);
+        
+    }
+    
+    return 0;
+}
+
